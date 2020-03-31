11d5e323eb34837936badf998fb66a89ac569e5e
Alex MacLean <macleanalex78@gmail.com>
2020-03-31 18:43:18 -0400

    Restructuring

--- src/main/java/com/herobrine/mod/blocks/ModBlockStates.java
+++
@@ -1,8 +0,0 @@
-package com.herobrine.mod.blocks;
-
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.properties.BlockStateProperties;
-
-public class ModBlockStates extends BlockStateProperties {
-    public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
-}
