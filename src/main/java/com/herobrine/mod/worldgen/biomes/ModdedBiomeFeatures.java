491781ceb9a3df26b934f3a38ffafb9d0777a767
Alex MacLean <macleanalex78@gmail.com>
2021-08-07 14:03:01 -0400

    Fixed Cursed Forest generating incorrectly. Cursed Forest is no longer considered ideal for player spawning to the game

--- src/main/java/com/herobrine/mod/worldgen/biomes/ModdedBiomeFeatures.java
+++ src/main/java/com/herobrine/mod/worldgen/biomes/ModdedBiomeFeatures.java
@@ -13,20 +13,20 @@ import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
+import org.jetbrains.annotations.NotNull;
 
 public class ModdedBiomeFeatures {
-    private static <FC extends IFeatureConfig> ConfiguredFeature<FC, ?> register(String key, ConfiguredFeature<FC, ?> configuredFeature) {
+    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_OAK_CONFIG = register("cursed_oak_config", Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.OAK_LOG.defaultBlockState()), new SimpleBlockStateProvider(Blocks.AIR.defaultBlockState()), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build()));
+    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_OAK_TREE = register("cursed_oak_tree", Feature.TREE.configured(CURSED_OAK_CONFIG.config()));
+    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_BIRCH_CONFIG = register("cursed_birch_config", Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.BIRCH_LOG.defaultBlockState()), new SimpleBlockStateProvider(Blocks.AIR.defaultBlockState()), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(5, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build()));
+    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_BIRCH_TREE = register("cursed_birch_tree", Feature.TREE.configured(CURSED_BIRCH_CONFIG.config()));
+    public static final ConfiguredFeature<?, ?> CURSED_TREES = register("cursed_trees", Feature.RANDOM_SELECTOR.configured(new MultipleRandomFeatureConfig(ImmutableList.of(CURSED_BIRCH_TREE.weighted(0.2F)), CURSED_OAK_TREE)).decorated(Features.Placements.HEIGHTMAP_SQUARE).decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(10, 0.1F, 1))));
+
+    private static <FC extends IFeatureConfig> @NotNull ConfiguredFeature<FC, ?> register(String key, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, HerobrineMod.MODID + ":" + key, configuredFeature);
     }
 
-    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_OAK_CONFIG = register("cursed_oak_config", Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.OAK_LOG.defaultBlockState()), new SimpleBlockStateProvider(Blocks.AIR.defaultBlockState()), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(4, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build()));
-    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_OAK_TREE = register("cursed_oak_tree", Feature.TREE.configured(CURSED_OAK_CONFIG.config));
-    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_BIRCH_CONFIG = register("cursed_birch_config", Feature.TREE.configured((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.BIRCH_LOG.defaultBlockState()), new SimpleBlockStateProvider(Blocks.AIR.defaultBlockState()), new BlobFoliagePlacer(FeatureSpread.fixed(2), FeatureSpread.fixed(0), 3), new StraightTrunkPlacer(5, 2, 0), new TwoLayerFeature(1, 0, 1))).ignoreVines().build()));
-    public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> CURSED_BIRCH_TREE = register("cursed_birch_tree", Feature.TREE.configured(CURSED_BIRCH_CONFIG.config));
-    public static final ConfiguredFeature<?, ?> CURSED_TREES = register("cursed_trees", Feature.RANDOM_SELECTOR.configured(new MultipleRandomFeatureConfig(ImmutableList.of(CURSED_BIRCH_TREE.weighted(0.2F)), CURSED_OAK_TREE)).decorated(Features.Placements.HEIGHTMAP).decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(10, 0.1F, 1))));
-
-
-    public static void withCursedTrees(BiomeGenerationSettings.Builder builder) {
+    public static void withCursedTrees(BiomeGenerationSettings.@NotNull Builder builder) {
         builder.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, CURSED_TREES);
     }
 }
--- src/main/java/com/herobrine/mod/worldgen/biomes/ModdedBiomeMaker.java
+++ src/main/java/com/herobrine/mod/worldgen/biomes/ModdedBiomeMaker.java
@@ -13,19 +13,19 @@ public class ModdedBiomeMaker {
     private static int getSkyColorWithTemperatureModifier(float temperature) {
         float lvt_1_1_ = temperature / 3.0F;
         lvt_1_1_ = MathHelper.clamp(lvt_1_1_, -1.0F, 1.0F);
-        return MathHelper.color(0.62222224F - lvt_1_1_ * 0.05F, 0.5F + lvt_1_1_ * 0.1F, 1.0F);
+        return MathHelper.hsvToRgb(0.62222224F - lvt_1_1_ * 0.05F, 0.5F + lvt_1_1_ * 0.1F, 1.0F);
     }
 
-    private static Biome makeGenericCursedForestBiome(float depth, float scale, MobSpawnInfo.Builder mobSpawnBuilder) {
-        BiomeGenerationSettings.Builder biomegenerationsettings$builder = (new BiomeGenerationSettings.Builder()).surfaceBuilder(ConfiguredSurfaceBuilders.NOPE);
+    private static @NotNull Biome makeGenericCursedForestBiome(float depth, float scale, MobSpawnInfo.@NotNull Builder mobSpawnBuilder) {
+        BiomeGenerationSettings.Builder biomegenerationsettings$builder = (new BiomeGenerationSettings.Builder()).surfaceBuilder(ConfiguredSurfaceBuilders.GRASS);
         DefaultBiomeFeatures.addDefaultOverworldLandStructures(biomegenerationsettings$builder);
         biomegenerationsettings$builder.addStructureStart(StructureFeatures.RUINED_PORTAL_STANDARD);
         DefaultBiomeFeatures.addDefaultCarvers(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultLakes(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultMonsterRoom(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addForestFlowers(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.addDefaultSoftDisks(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultUndergroundVariety(biomegenerationsettings$builder);
+        DefaultBiomeFeatures.addDefaultOres(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultSoftDisks(biomegenerationsettings$builder);
         ModdedBiomeFeatures.withCursedTrees(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultFlowers(biomegenerationsettings$builder);
@@ -34,9 +34,8 @@ public class ModdedBiomeMaker {
         DefaultBiomeFeatures.addDefaultExtraVegetation(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addDefaultSprings(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addSurfaceFreezing(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.addExtraGold(biomegenerationsettings$builder);
-        DefaultBiomeFeatures.addWaterTrees(biomegenerationsettings$builder);
         DefaultBiomeFeatures.addExtraEmeralds(biomegenerationsettings$builder);
+        DefaultBiomeFeatures.addExtraGold(biomegenerationsettings$builder);
         return (new Biome.Builder()).precipitation(Biome.RainType.RAIN).biomeCategory(Biome.Category.FOREST).depth(depth).scale(scale).temperature(0.7F).downfall(0.8F).specialEffects((new BiomeAmbience.Builder()).waterColor(4159204).waterFogColor(329011).fogColor(12638463).foliageColorOverride(0x9EA94D).grassColorOverride(0x90A94D).skyColor(getSkyColorWithTemperatureModifier(0.7F)).ambientMoodSound(MoodSoundAmbience.LEGACY_CAVE_SETTINGS).build()).mobSpawnSettings(mobSpawnBuilder.build()).generationSettings(biomegenerationsettings$builder.build()).build();
     }
 
