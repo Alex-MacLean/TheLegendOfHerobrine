3fe33cda9af426dae1c9d5816ad12153e16bc2ea
Alex MacLean <macleanalex78@gmail.com>
2021-11-12 16:45:07 -0500

    Create Fabric 1.17.1 Branch

--- src/main/java/com/herobrine/mod/util/blocks/ModBlockStates.java
+++
@@ -1,8 +0,0 @@
-package com.herobrine.mod.util.blocks;
-
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.properties.BlockStateProperties;
-
-public class ModBlockStates extends BlockStateProperties {
-    public static final IntegerProperty TYPE = IntegerProperty.create("type", 0, 2);
-}
--- src/main/java/com/herobrine/mod/util/loot_tables/LootTableInit.java
+++
@@ -1,8 +0,0 @@
-package com.herobrine.mod.util.loot_tables;
-
-import com.herobrine.mod.HerobrineMod;
-import net.minecraft.util.ResourceLocation;
-
-public class LootTableInit {
-    public static final ResourceLocation HEROBRINE = HerobrineMod.location("entities/herobrine");
-}
