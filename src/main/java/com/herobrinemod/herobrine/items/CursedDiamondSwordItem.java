d31fd43d8a6fb5f2c133ed91cbe00dbc1c5804f4
Alex-MacLean <macleanalex78@gmail.com>
2023-07-08 11:58:36 -0700

    *Cursed Diamond Sword is now charged by kills, the amount of kills required to charge is configurable, and the default cooldown has been cut in half *The lightning strike attack for the Cursed Diamond Sword can now be cancelled by sneaking *Tick delta for the raycast for the Cursed Diamond Sword lightning strike attack has been changed from 1 to 0 *Fixed broken swords item tag entry for Cursed Diamond Sword *Updated supported languages to contain all translations. May not be 100% accurate

--- src/main/java/com/herobrinemod/herobrine/items/CursedDiamondSwordItem.java
+++ src/main/java/com/herobrinemod/herobrine/items/CursedDiamondSwordItem.java
@@ -1,45 +1,74 @@
 package com.herobrinemod.herobrine.items;
 
 import com.herobrinemod.herobrine.savedata.ConfigHandler;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CursedDiamondSwordItem extends SwordItem {
+    private int kills;
     public CursedDiamondSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(@NotNull World world, @NotNull PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ILLUSIONER_CAST_SPELL, SoundCategory.PLAYERS, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
-        for (int i = 0; i < 16; i ++) {
-            world.addParticle(ParticleTypes.PORTAL, user.getParticleX(1.0), user.getRandomBodyY() - 1, user.getParticleZ(1.0), user.getRandom().nextGaussian() * 0.02, user.getRandom().nextGaussian() * 0.02, user.getRandom().nextGaussian() * 0.02);
+    public boolean postHit(ItemStack stack, @NotNull LivingEntity target, LivingEntity attacker) {
+        if(!target.isAlive()) {
+            this.kills ++;
         }
-        if(!user.getItemCooldownManager().isCoolingDown(this)) {
-            Vec3d pos = user.raycast(16.0, 1.0f, false).getPos();
+        return super.postHit(stack, target, attacker);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, @NotNull List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.herobrine.cursed_diamond_sword.kills").append(ScreenTexts.SPACE).append(String.valueOf(this.kills)));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(@NotNull World world, @NotNull PlayerEntity user, Hand hand) {
+        if((user.isCreative() || this.kills >= ConfigHandler.getHerobrineConfig().readInt("CursedDiamondSwordChargeCost")) && !user.isSneaking()) {
+            ItemStack itemStack = user.getStackInHand(hand);
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ILLUSIONER_CAST_SPELL, SoundCategory.PLAYERS, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+            for (int i = 0; i < 16; i ++) {
+                world.addParticle(ParticleTypes.PORTAL, user.getParticleX(1.0), user.getRandomBodyY() - 1, user.getParticleZ(1.0), user.getRandom().nextGaussian() * 0.02, user.getRandom().nextGaussian() * 0.02, user.getRandom().nextGaussian() * 0.02);
+            }
+            Vec3d pos = user.raycast(16.0, 0.0f, false).getPos();
             LightningEntity lightningentity = EntityType.LIGHTNING_BOLT.create(world);
             assert lightningentity != null;
             lightningentity.setPos(pos.getX(), pos.getY(), pos.getZ());
             world.spawnEntity(lightningentity);
             user.incrementStat(Stats.USED.getOrCreateStat(this));
-            if (!user.getAbilities().creativeMode) {
+            if (!user.isCreative()) {
                 user.getItemCooldownManager().set(this, ConfigHandler.getHerobrineConfig().readInt("CursedDiamondSwordMagicCooldown"));
-                itemStack.damage(ConfigHandler.getHerobrineConfig().readInt("CursedDiamondMagicItemDamage"), user, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                if(hand.equals(Hand.MAIN_HAND)) {
+                    itemStack.damage(ConfigHandler.getHerobrineConfig().readInt("CursedDiamondMagicItemDamage"), user, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                } else {
+                    itemStack.damage(ConfigHandler.getHerobrineConfig().readInt("CursedDiamondMagicItemDamage"), user, e -> e.sendEquipmentBreakStatus(EquipmentSlot.OFFHAND));
+                }
+                if(!world.isClient) {
+                    this.kills = kills - ConfigHandler.getHerobrineConfig().readInt("CursedDiamondSwordChargeCost");
+                }
             }
             return TypedActionResult.success(itemStack, world.isClient());
         }
